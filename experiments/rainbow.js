2315bbdcf7a190837815def6e9968ba78516ef3c
Alma <sial23hk@student.ju.se>
2025-09-03 18:17:16 +0200

    tweaked numbers, renamed my js files to fit better

--- experiments/rainbow.js
+++ experiments/rainbow.js
@@ -1,34 +1,37 @@
 // citation, base of code is derived from this link: https://www.gorillasun.de/blog/radial-perlin-noise-and-generative-tree-rings/
 
+
 function setup() {
   createCanvas(innerWidth, innerHeight);
- background(249, 202, 24); 
-  stroke(20);
+  background(0);
   strokeWeight(1);
   noFill();
-  frameRate(60);
+  frameRate(30);
+
+  colorMode(HSB, 360, 100, 100); // HSB color mode here
 }
 
 let scale = 50;
-// tweaked res to get the shape more round
-let resolution = 0.0001;
+//changed res for the strokes to change shape based on how i liked the outcomes the most
+let resolution = 0.00969;
 let numPoints = 500;
-
 let radius = 150;
-let numRings = 40;
-function draw() {
-  translate(width / 2, height / 2); // center my shape
+let numRings = 50;
 
-  //tweaked numbers here to get it to move like a cd more
+function draw() {
+  translate(width / 2, height / 2);  // center my shape
+
+    //tweaked numbers here to get it to look like a cd more
   let offsetX = map(noise(frameCount * 0.01), 0, 3, -5, 5);
   let offsetY = map(noise(frameCount * 0.01 + 100), 0, 1, -5, 5);
 
   for (let r = 0; r < radius; r += radius / numRings) {
     // added cd colours between these strokes
-    if (r < radius * 0.1 || r > radius * 0.3) {
-      stroke(0); // black
+    if (r < radius * 0.01 || r > radius * 0.01) {
+      let randomHue = Math.random() * 360; 
+      stroke(randomHue, 100, 100); 
     } else {
-      stroke(255); // white
+      stroke(0);
     }
 
     beginShape();
@@ -39,21 +42,14 @@ function draw() {
     ) {
       let x = r * cos(a) + offsetX;
       let y = r * sin(a) + offsetY;
-
       let n = map(noise(x * resolution, y * resolution), 0, 1, -scale, scale);
       curveVertex(x + n, y + n);
-      // did not want it as randomized so removed this part
-      // if (random() > 0.75 - 0.25 * sin(r)) {
-      //   endShape();
-      //     beginShape();
-      // }
     }
     endShape();
   }
+ //  the following lines of code is derived from: https://p5js.org/reference/p5/frameCount/
 
-  //  the following lines of code is derived from: https://p5js.org/reference/p5/frameCount/
-
-  if (frameCount > 100) {
-    noLoop(); // stops animation after 100 frames, freezes
+  if (frameCount > 30) {
+    noLoop(); // stops animation
   }
 }
